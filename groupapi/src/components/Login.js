d0c8c8a32394b063e28b1e3c1ce56695c86ab814
mizan98 <56539288+mizan98@users.noreply.github.com>
2019-11-20 15:08:11 +0000

    Add files via upload

---
+++ groupapi/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+    <div>
+        <input type='email' name='email'></input>
+        <input type='password' name='password'></input>
+        <button>Login</button>
+    </div>
+    )
+}
+
