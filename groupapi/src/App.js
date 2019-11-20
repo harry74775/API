8a02742dab9843783afb12180a01c1917722b4ec
james-poolie <56539323+james-poolie@users.noreply.github.com>
2019-11-20 11:49:52 +0000

    Bojo

--- groupapi/src/App.js
+++ groupapi/src/App.js
@@ -1,11 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path='/' render={() =>
+          <div>
+            sign in / up
+            {/* SIGN IN / SIGN UP */}
+          </div>
+          }/>
+          <Route exact path='/Home' render={() =>
+          <div>
+            <Navigation />
+            Home
+          </div>
+          }/>
+          <Route exact path='/One' render={() =>
+          <div>
+            <Navigation />
+            1
+            {/* API ONE SHIT */}
+          </div>
+          }/>
+          <Route exact path='/Two' render={() =>
+          <div>
+            <Navigation />
+            2
+            {/* API TWO SHIT */}
+          </div>
+          }/>
+          <Route exact path='/Three' render={() =>
+          <div>
+            <Navigation />
+            3
+            {/* API THREE SHIT */}
+          </div>
+          }/>
+          <Route exact path='/Four' render={() =>
+          <div>
+            <Navigation />
+            4
+            {/* API FOUR SHIT */}
+          </div>
+          }/>
+          <Route exact path='/Five' render={() =>
+          <div>
+            <Navigation />
+            5
+            {/* API FIVE SHIT */}
+          </div>
+          }/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
